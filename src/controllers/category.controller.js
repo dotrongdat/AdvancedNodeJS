44c48eb15b36cff86f2030d9934179ec4aeac9a0
DESKTOP-4BHH352\Win <datdt.dev.work@gmail.com>
2022-08-13 23:04:54 +0700

    [CategoryController]: [getMethod] - change message return to 'Get successfully v2'

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -6,7 +6,7 @@ const get = async (req, res) => {
 	try {
 		res.status(StatusCodes.OK).json({
 			payload: { categories: await categoryService.get() },
-			message: 'Get successfully',
+			message: 'Get successfully v2',
 		})
 	} catch (error) {
 		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
